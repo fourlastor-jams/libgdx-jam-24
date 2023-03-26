4ae1604727a5b8bca036542f439e7bc0bda2d02b
Daniele Conti <fourlastor@users.noreply.github.com>
2023-03-26 21:29:17 +0100

    Show loading screen

--- html/src/main/java/io/github/fourlastor/game/client/HtmlLauncher.java
+++ html/src/main/java/io/github/fourlastor/game/client/HtmlLauncher.java
@@ -3,6 +3,8 @@ package io.github.fourlastor.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.core.client.GWT;
 import io.github.fourlastor.game.GdxGame;
 
 public class HtmlLauncher extends GwtApplication {
@@ -17,6 +19,12 @@ public class HtmlLauncher extends GwtApplication {
                 // Fixed size application:
                 //return new GwtApplicationConfiguration(480, 320);
         }
+
+        @Override
+        public Preloader.PreloaderCallback getPreloaderCallback() {
+                return createPreloaderPanel(GWT.getHostPageBaseURL() + "preloadlogo.png");
+        }
+
 
         @Override
         public ApplicationListener createApplicationListener () {
